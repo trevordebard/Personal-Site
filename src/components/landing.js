0bd93258c3af0f7bf6945dd00605048fe855c496
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2020-02-01 11:40:34 -0600

    Add scrolling arrow to landing

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { useSpring, animated } from 'react-spring';
+import { useInView } from 'react-intersection-observer';
 
 import { graphql, useStaticQuery } from 'gatsby';
 
@@ -43,6 +44,27 @@ const Links = styled(animated.div)`
     font-weight: 300;
   }
 `;
+const Arrow = styled.i`
+  border: solid white;
+  border-width: 0 1px 1px 0;
+  display: inline-block;
+  transform: rotate(45deg);
+  padding: 10px;
+  position: absolute;
+  bottom: 15px;
+  opacity: 1;
+  animation: fade 3s infinite linear;
+
+  @keyframes fade {
+    0%,
+    100% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+  }
+`;
 export default () => {
   const data = useStaticQuery(graphql`
     {
@@ -57,6 +79,7 @@ export default () => {
     from: { marginTop: 300 },
     delay: 100,
   });
+  const [arrowRef, inView] = useInView({ rootMargin: '-100px' });
   return (
     <LandingContainer>
       <Header style={props}>
@@ -89,6 +112,7 @@ export default () => {
             </li>
           </ul>
         </Links>
+        {!inView && <Arrow ref={arrowRef} />}
       </Header>
     </LandingContainer>
   );
